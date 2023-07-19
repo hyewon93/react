0f0290325dbd7ad5b7394dd0dfaff00ef281d310
hyewon93 <baehw93@gmail.com>
2023-07-19 16:27:21 -0700

    Journal Detail Page

--- real-journal/src/components/EmotionItem.js
+++ real-journal/src/components/EmotionItem.js
@@ -1,19 +1,18 @@
 import "./EmotionItem.css";
 import Card from 'react-bootstrap/Card';
 
-const EmotionItem = ({score , text, innerRef, onClick}) => {
+const EmotionItem = ({score , text, active, innerRef, onClick}) => {
+
+    const activeClass = active ? active : "active";
 
     return (
-        <div>
-            <div className="emotion_wrapper active" score={score} ref={innerRef} onClick={onClick}>
-                <Card className="emotion_card" >
-                    <Card.Img variant="top" src={"img/emotion_" + score + ".png"} />
-                    <Card.Body>
-                        <Card.Text>{text}</Card.Text>
-                    </Card.Body>
-                </Card>
-                <div className="emotion_cover"></div>
-            </div>
+        <div className={"emotion_wrapper " + activeClass} score={score} ref={innerRef} onClick={onClick}>
+            <Card className="emotion_card" >
+                <Card.Img variant="top" src={"img/emotion_" + score + ".png"} />
+                <Card.Body>
+                    <Card.Text>{text}</Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
--- real-journal/src/pages/Journal.js
+++ real-journal/src/pages/Journal.js
@@ -1,18 +1,69 @@
+import { useContext, useEffect, useState } from "react";
 import MyHeader from "../components/MyHeader";
-import MyButton from "../components/MyButton";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { JournalStateContext } from "../App";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Button from "react-bootstrap/esm/Button";
+import './page.css';
+import EmotionItem from "../components/EmotionItem";
 
 const Journal = () => {
 
+    const [originData, setOriginData] = useState();
     const [searchParams] = useSearchParams();
-
     const id = searchParams.get("id");
+    const journalList = useContext(JournalStateContext);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(journalList.length>=1){
+          const targetJournal = journalList.find((it)=>parseInt(it.id) === parseInt(id));
+
+          if(targetJournal) {
+            setOriginData(targetJournal);
+          }
+          else {
+            navigate('/', {replace:true});
+          }
+        }
+      }, [id, journalList, navigate]);
+    
 
     return (
         <div>
-            <MyHeader headText="Journal" leftChild={<MyButton text="<" onClick=""/>} rightChild={<MyButton text="Edit" type="positive" onClick=""/>}/>
-            <h1>Journal</h1>
-            <p>{id}</p>
+            <MyHeader headText="Journal"/>
+            <Container className="journal-details">
+                <Row>
+                    <Col>
+                        <h3>Date</h3>
+                        <span className="journal-detail">{originData && originData.date}</span>
+                    </Col>
+                </Row>
+                <Row className="emotion-container">
+                    <Col>
+                        <h3>Emotion</h3>
+                        <Container className="journal-detail">
+                            <Row>
+                                <Col><EmotionItem score="5" text="Very Good" active="inactive"/></Col>
+                                <Col><EmotionItem score="4" text="Good" active="active"/></Col>
+                                <Col><EmotionItem score="3" text="Normal" active="inactive"/></Col>
+                                <Col><EmotionItem score="2" text="Bad" active="inactive"/></Col>
+                                <Col><EmotionItem score="1" text="Very Bad" active="inactive"/></Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h3>Content</h3>
+                        <p className="journal-detail content-container">{originData && originData.content}</p>
+                    </Col>
+                </Row>
+            </Container>
+            <Button className="margin-right-40" type="button" variant="light" onClick={() => {navigate("/")}}>Back</Button>
+            <Button type="button" onClick={() => {navigate("/edit?id" + originData.id)}}>Edit</Button>
         </div>
     );
 }
